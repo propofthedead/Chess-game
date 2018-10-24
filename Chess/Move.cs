7b0ed215d2b8890d6653374eb194bcf90df0e380
Foster Stule <camodude5@yahoo.com>
2018-10-24 14:53:52 -0400

    added a piece selector based on xy coordinates, and move it on a choice of xy
    added a boolean to test if right move

--- Chess/Move.cs
+++ Chess/Move.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace Chess
 {
-	class Move
+	public class Move: Pawn
 	{
+		private int targetX;
+		private int targetY;
+		private int destinationX;
+		private int destinationY;
+
+		public Move()
+		{
+			targetX = 0;
+			targetY = 0;
+			destinationX = 0;
+			destinationY = 0;
+
+			Exit = false;
+
+		}
+
+		public bool Exit { get; set; }
 	}
 }
