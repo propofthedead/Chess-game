456c3d95e3f9ad0efcc674fba2cb2318d7b3c783
Foster Stule <camodude5@yahoo.com>
2018-10-24 14:41:07 -0400

    added a chessboard class with population of a pawn and a move

---
+++ Chess/ChessBoard.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess
+{
+	public class ChessBoard
+	{
+		private string[,] chessBoard;
+		public const int DIMENSION = 8; // orginal 8x8 board
+
+		private Pawn pawn;
+		private Move move;
+
+		public ChessBoard()
+		{
+			pawn = new Pawn();
+			move = new Move();
+			chessBoard = new string[DIMENSION, DIMENSION];
+			ChessBoardHorizontalSymbol = "+---";
+			ChessBoardVerticalSymbol = "| ";
+
+		}
+
+		public string ChessBoardHorizontalSymbol { get; set; }
+		public string ChessBoardVerticalSymbol { get; set; }
+	}
+}
