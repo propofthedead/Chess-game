5909a6a5794a6196a3448f9195d0699b5d2aeaa4
Foster Stule <camodude5@yahoo.com>
2018-10-24 16:19:57 -0400

    added a display fuction that uses a combination of for loops to print out a board

--- Chess/ChessBoard.cs
+++ Chess/ChessBoard.cs
@@ -11,12 +11,11 @@ namespace Chess
 		private string[,] chessBoard;
 		public const int DIMENSION = 8; // orginal 8x8 board
 
-		private Pawn pawn;
+		
 		private Move move;
 
 		public ChessBoard()
 		{
-			pawn = new Pawn();
 			move = new Move();
 			chessBoard = new string[DIMENSION, DIMENSION];
 			ChessBoardHorizontalSymbol = "+---";
@@ -26,5 +25,28 @@ namespace Chess
 
 		public string ChessBoardHorizontalSymbol { get; set; }
 		public string ChessBoardVerticalSymbol { get; set; }
+
+		public void displayChessBoard()
+		{
+			//while (!move.Exit) {
+				Console.Clear();
+				Console.WriteLine("    0   1   2   3   4   5   6   7"); // x axis header
+
+				for(int r=0; r < DIMENSION; r++) {
+					Console.Write("  ");// left spacing- 2 spaces
+					for(int c=0; c < DIMENSION; c++) {
+						Console.Write(ChessBoardHorizontalSymbol); // write the horizonstal line
+					}
+
+					Console.Write("+\n");
+					for( int c=0; c < DIMENSION; c++) {
+						Console.Write(r + " "); //y axis header
+						Console.Write(ChessBoardVerticalSymbol + Pawn.pawns[r, c] + " "); // displays pawns
+					}
+					Console.Write("|\n");
+				}
+
+			//}
+		}
 	}
 }
