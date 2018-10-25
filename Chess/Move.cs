b19416b625499437726dc87374d737b302383a7e
Foster Stule <camodude5@yahoo.com>
2018-10-25 13:49:35 -0400

    added three methods to allow movement of the pawns and check for valid coordiantes

--- Chess/Move.cs
+++ Chess/Move.cs
@@ -25,5 +25,57 @@ namespace Chess
 		}
 
 		public bool Exit { get; set; }
+
+		public void MakeMove()
+		{
+			getInput();
+
+			rearrangePawns();
+		}
+
+		private void getInput()
+		{
+			//get input and validate it
+			Console.WriteLine("Enter Targets X axis");
+			Exit = validateInput(int.TryParse(Console.ReadLine(), out targetX));
+
+
+			if (!Exit) {
+				Console.WriteLine("Enter Targets Y axis");
+				Exit = validateInput(int.TryParse(Console.ReadLine(), out targetY));
+			}
+			if (!Exit) {
+				Console.WriteLine("Enter Destination's X axis");
+				Exit = validateInput(int.TryParse(Console.ReadLine(), out destinationX));
+			}
+			if (!Exit) {
+				Console.WriteLine("Enter Destination's Y axis");
+				Exit = validateInput(int.TryParse(Console.ReadLine(), out destinationY));
+			}
+
+		}
+
+		private bool validateInput( bool parsed)
+		{
+			bool error = false;
+			if (!parsed)
+				error = true;
+			else if (targetX < 0 || targetY < 0 || destinationX < 0 || destinationY < 0)
+				error = true;
+			else if (targetX > ChessBoard.DIMENSION - 1 || targetY > ChessBoard.DIMENSION - 1 || destinationX > ChessBoard.DIMENSION - 1 || destinationY > ChessBoard.DIMENSION - 1)
+				error = true;
+
+
+			if (error)
+				Console.WriteLine("Invalid input. Exiting program now!");
+
+			return error;
+		}
+
+		private void rearrangePawns()
+		{
+			pawns[destinationX, destinationY] = pawns[targetX, targetY];
+			pawns[targetX, targetY] = SPACE;
+		}
 	}
 }
