325b6fd617592d086c683e20d908268be7496c6f
Foster Stule <camodude5@yahoo.com>
2018-10-25 12:47:42 -0400

    added a initlize pawn fuction to fill the 2-d array with P's and spaces

--- Chess/Pawn.cs
+++ Chess/Pawn.cs
@@ -15,6 +15,23 @@ namespace Chess
 		public Pawn()
 		{
 			pawns = new char[ChessBoard.DIMENSION, ChessBoard.DIMENSION];
+			initPawn(); //populate pawn array with spaces and P's
+
+		}
+
+		private void initPawn()
+		{
+			for (int r = 0; r < ChessBoard.DIMENSION; r++) {
+				for(int c=0; c<ChessBoard.DIMENSION; c++) {
+					//place Pawns in the first and last two rows of the array
+					if (r == 0 || r == 1 || r == ChessBoard.DIMENSION - 2 || r == ChessBoard.DIMENSION - 1)
+						pawns[r, c] = PAWN_SYMBBOL;
+					else
+						pawns[r, c] = SPACE;
+
+					
+				}
+			}
 		}
 	}
 }
